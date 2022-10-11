122612724eb3134fb44ab36623285973b3a36f2d
tyrion <vu.van.phong@moneyforward.vn>
2022-10-11 13:44:57 +0700

    init base

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 5000
+const URI = process.env.DATABASE_URL
+
+// middleware
+app.use(bodyParser.json({ limit: '30mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }))
+app.use(cors())
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB')
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.error(err)
+  })
